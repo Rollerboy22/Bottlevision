bab63af09e742300be98831a8606d009fa7d8c21
Rollerboy22 <utrusted1@gmail.com>
2026-09-16 04:54:39 +0300

    Expose SAM 3 segmentation adapter

--- src/bottle_vision/segmentation/__init__.py
+++ src/bottle_vision/segmentation/__init__.py
@@ -2,10 +2,12 @@
 
 from .base import Segmenter, UnconfiguredSegmenter, build_segmenter
 from .quality import score_mask
+from .sam3 import Sam3Segmenter
 from .types import MaskQuality, SegmentationInstance, SegmentationResult
 
 __all__ = [
     "MaskQuality",
+    "Sam3Segmenter",
     "Segmenter",
     "SegmentationInstance",
     "SegmentationResult",
